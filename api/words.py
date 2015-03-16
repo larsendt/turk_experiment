263e8c4c596a40d5bbbad4c13fff0d5104b7606f
Dane Larsen <dane@larsendt.com>
2015-03-16 04:19:43 -0400

    word blocks are working

--- api/words.py
+++ api/words.py
@@ -1,8 +1,12 @@
+import json
+import copy
+import random
+
+with open("../turk_experiment_blocks.json", "r") as f:
+    BLOCKS = json.load(f)["words"]
 
 def get_word_blocks():
-    return {"warmup": ["lorem", "ipsum"],
-            "block1": ["a", "b", "c"],
-            "block2": ["d", "e", "f"],
-            "block3": ["g", "h", "i"],
-            "block4": ["j", "k", "l"],
-            "block5": ["m", "n", "o"]}
+    blocks = copy.deepcopy(BLOCKS)
+    for block_name in blocks:
+        random.shuffle(blocks[block_name])
+    return blocks
