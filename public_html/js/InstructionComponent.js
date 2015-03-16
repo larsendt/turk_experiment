97a15b4d7eaff5175ab9451580c237fe1ea641cd
Dane Larsen <dane@larsendt.com>
2015-03-16 01:10:11 -0400

    mostly finished instructions JS module

--- public_html/js/InstructionComponent.js
+++ public_html/js/InstructionComponent.js
@@ -14,26 +14,30 @@ var InstructionComponent = React.createClass({
                 this.setState({instructions: data});
             }.bind(this),
             error: function(err) {
-                this.setState({error: "NUUUUUU"});
+                this.setState({error: "Failed to fetch instructions!"});
             }.bind(this),
         });
     },
     previousInstruction: function(event) {
         var prev = this.state.cur_instr - 1;
-        if(prev < 0) {
-            prev = 0;
+        if(prev >= 0) {
+            this.setState({cur_instr: prev});
         }
-        this.setState({cur_instr: prev});
         event.preventDefault();
     },
     nextInstruction: function(event) {
         var next = this.state.cur_instr + 1;
-        if(next >= this.state.instructions.length) {
-            this.props.finishInstructions();
-        }
-        else {
+        if(next < this.state.instructions.length) {
             this.setState({cur_instr: next});
         }
+        event.preventDefault();
+    },
+    accept: function(event) {
+        this.props.acceptAgreement();
+        event.preventDefault();
+    },
+    decline: function(event) {
+        this.props.declineAgreement();
         event.preventDefault();
     },
     render: function() {
@@ -55,10 +59,12 @@ var InstructionComponent = React.createClass({
         }
 
         var next_style = {};
-        var accept_style = {};
-        var decline_style = {};
-        if(this.state.cur_instr >= this.state.instructions.length-1) {
-            next_style.display = "none";
+        var accept_style = {color: "#030", backgroundColor: "#0f0", border: "1px solid #050"};
+        var decline_style = {color: "#300", backgroundColor: "#f00", border: "1px solid #500"};
+        if(this.state.instructions.length > 0 && this.state.cur_instr >= this.state.instructions.length-1) {
+            next_style.backgroundColor = "#ccc";
+            next_style.border = "1px solid #777";
+            next_style.color = "#777";
         }
         else {
             accept_style.display = "none";
@@ -66,48 +72,45 @@ var InstructionComponent = React.createClass({
         }
 
         var prev_elem = (
-            <div id="instruction-button" className="generic-button"
-                 onClick={this.nextInstruction} style={prev_style}>
+            <div className="instruction-button generic-button"
+                 onClick={this.previousInstruction} style={prev_style}>
                 <span><i className="fa fa-arrow-left" /> Prev</span>
             </div>
         );
 
         var next_elem = (
-            <div id="instruction-button" className="generic-button"
+            <div className="instruction-button generic-button"
                  onClick={this.nextInstruction} style={next_style}>
                 <span>Next <i className="fa fa-arrow-right" /></span>
             </div>
         );
 
         var decline_elem = (
-            <div id="instruction-button" className="generic-button"
-                 onClick={this.nextInstruction} style={decline_style}>
+            <div className="instruction-button generic-button"
+                 onClick={this.decline} style={decline_style}>
                 <span>Decline <i className="fa fa-times" /></span>
             </div>
         );
 
         var accept_elem = (
-            <div id="instruction-button" className="generic-button"
-                 onClick={this.nextInstruction} style={accept_style}>
+            <div className="instruction-button generic-button"
+                 onClick={this.accept} style={accept_style}>
                 <span>Accept <i className="fa fa-check" /></span>
             </div>
         );
 
         return (
             <div className="instruction-wrapper">
-                {err_elem}
                 <div className="instruction-text">
                     <img id="logo" src="img/logo.png" />
+                    {err_elem}
                     <p>{instr}</p>
                 </div>
                 <div id="instruction-buttons">
-                    <div id="instruction-button" className="generic-button"
-                         onClick={this.previousInstruction} style={prev_style}>
-                    </div>
-                     {prev_elem}
-                     {next_elem}
                      {decline_elem}
                      {accept_elem}
+                     {prev_elem}
+                     {next_elem}
                 </div>
             </div>
         );
